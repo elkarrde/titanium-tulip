c7fa7d73d1ea5b79de417e789bd5b3d3501c9f72
elkarrde <nfalica@gmail.com>
2018-05-17 14:22:04 +0200

    Main components, landing and products.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(),
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/products-adapter.ts
@@ -0,0 +1,18 @@
+import { Product } from './product';
+
+export const PRODUCTS: Product[] = [
+  { id: 11, name: 'Premium Zataar Spice', price: 22.5, vendor: 'SpiceMarket, Zagreb', category: 'spice' },
+  { id: 12, name: 'Premium Zataar Spice', price: 23.3, vendor: 'UltraGoodDeal, Zagreb', category: 'spice' },
+  { id: 13, name: 'Ordinary Zataar Plus', price: 21.4, vendor: 'SpiceMarket, Zagreb', category: 'spice' },
+  { id: 14, name: 'Ordinary Zataar Plus', price: 22.1, vendor: 'UltraGoodDeal, Zagreb', category: 'spice' },
+
+  { id: 21, name: 'VeryFine Couscous, 500g', price: 31.9, vendor: 'UltraGoodDeal, Zagreb', category: 'food' },
+  { id: 22, name: 'VeryFine Couscous, 500g', price: 31.9, vendor: 'Fine Foods Everywhere, Zagreb', category: 'food' },
+  { id: 23, name: 'JustGood Couscous, 250g', price: 17.9, vendor: 'UltraGoodDeal, Zagreb', category: 'food' },
+  { id: 24, name: 'JustGood Couscous, 250g', price: 16.9, vendor: 'Fine Foods Everywhere, Zagreb', category: 'food' },
+
+  { id: 31, name: 'Super 95', price: 10.3, vendor: 'INA, Zagreb', category: 'fuel' },
+  { id: 32, name: 'Super 95', price: 10.4, vendor: 'LukOil, Zagreb', category: 'fuel' },
+  { id: 33, name: 'Super 95', price: 10.2, vendor: 'Crodux, Zagreb', category: 'fuel' },
+  { id: 33, name: 'Super 95', price: 10.4, vendor: 'Petrol, Zagreb', category: 'fuel' }
+];
